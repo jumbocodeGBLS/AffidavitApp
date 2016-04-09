5674c669862d568a6f01427dabaaaf2447e8d337
Elizabeth <Elizabeth@Elizabeths-MacBook-Air-4.local>
2016-04-09 10:40:14 -0400

    added links to clientview, clientlist, login

--- index.js
+++ index.js
@@ -15,11 +15,24 @@ app.get('/', function(request, response) {
  	response.send("<p>home page</p>");
 });
 
+app.get('/login', function(request,response) {
+	console.log("here i am");
+	response.sendFile(path.join(__dirname, '/index.html'));
+});
+
 app.get('/admin', function(request,response) {
 	response.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.get('/history', function(request,response) {
+	response.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/clientview', function(request,response) {
+	response.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/clientlist', function(request,response) {
 	response.sendFile(path.join(__dirname, '/index.html'));
 });
 
