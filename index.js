511a09ca360eb0c0e794d01ba84fad8cd490fc27
Ali Boreiko <alison.boreiko@tufts.edu>
2016-05-04 21:53:47 -0400

    all backend APIs working

--- index.js
+++ index.js
@@ -46,14 +46,10 @@ app.get('/clientlistData', function(request, response) {
 			// TODO: handle error
 		}
 	});
-	// clients for all lawyers
-	//var queryStr = "SELECT fname, progress, user_id FROM App_User WHERE user_id in \
-	//				(SELECT viewee FROM Client_Access WHERE viewer in \
-	//				(SELECT user_id FROM App_User WHERE type = 'lawyer'))";
 
 	// clients for a specific lawyer, lawyer ID hardcoded for now, expecting it from frontend
 	var queryStr = "SELECT fname, progress, user_id FROM App_User WHERE user_id in \
-					(SELECT viewee FROM Client_Access WHERE viewer = " + 3 + ")"; // + req.query.lawyerID + ")";
+					(SELECT viewee FROM Client_Access WHERE viewer = " + request.query.lawyerID + ")";
 	var query = client.query(queryStr, function(err, res) {
 		if (err) {
 			console.log(err);
@@ -73,11 +69,11 @@ app.get('/userData', function(request,response) {
 			// TODO: handle error
 		}
 	});
-	var queryStr = 'SELECT App_User.*, Client_Access.viewee' +
-					'FROM App_User' +
-					'LEFT JOIN Client_Access' + 
-						'ON App_User.user_id = Client_Access.viewer' + 
-					'GROUP BY App_User.id'; // + req.query.lawyerID + ')';
+  var queryStr = "SELECT App_User.*, ARRAY_AGG(Client_Access.viewee) AS viewee FROM App_User \
+        LEFT JOIN Client_Access \
+        ON App_User.user_id = Client_Access.viewer \
+        WHERE App_User.user_id =" + request.query.userID +
+        "GROUP BY App_User.user_id;"
 	var query = client.query(queryStr, function(err, res) {
 		if (err) {
 			console.log(err);
@@ -89,8 +85,28 @@ app.get('/userData', function(request,response) {
 	query.on('end', function() { client.end(); });
 });
 
-app.get('/history', function(request,response) {
-	response.sendFile(path.join(__dirname, '/index.html'));
+// Given a userID, return response history
+app.get('/historyData', function(request,response) {
+	client.connect(function(err) {
+		if (err) {
+			console.log(err);
+			// TODO: handle error
+		}
+	});
+  var queryStr = "SELECT q_id, txt, ARRAY_AGG(time) as date, ARRAY_AGG(recording_url) as link \
+                  FROM response LEFT JOIN question ON question.q_id=response.q_num \
+                  WHERE response.affidavit = (SELECT aff_id FROM affidavit WHERE client ="
+                  + request.query.clientID + ") \
+                  GROUP BY q_id;"
+  var query = client.query(queryStr, function(err, res) {
+		if (err) {
+			console.log(err);
+			// TODO: handle error
+		} else {
+			response.send(res.rows);
+		}
+	});
+	query.on('end', function() { client.end(); });
 });
 
 app.get('/clientview', function(request,response) {
