d412f4a1c88e64207ac96942bf527001b2260e0f
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2016-05-04 21:49:17 -0400

    dealt with parallel arrays

--- newcode/historyCtrl.js
+++ newcode/historyCtrl.js
@@ -1,15 +1,16 @@
 myapp.controller('historyCtrl', function($scope, $http) {                
     /*$scope.data = [
         {'question': 1, 'text': 'Whats up?', 
-        'responses': [{'date': '10/12/2015', 'link':'1'},
-                      {'date': '10/13/2015', 'link':'2'}]},
+        'dates': ['10/12/2015', '10/13/2015'],
+        'links': [1,2]},
         {'question': 2, 'text': 'How are you?', 
-        'responses': [{'date':'10/19/2015', 'link':'3'},
-                      {'date': '10/30/2015', 'link':'4'}]},
-        {'question': 3, 'text': 'How is it going?', 
-        'responses': [{'date':'11/12/2015', 'link':'5'}, 
-                      {'date': '11/14/2015', 'link':'6'}]}
+        'dates': ['10/19/2015', '10/30/2015'],
+        'links': [3,4]},
+        {'question': 3, 'text': 'How is it going', 
+        'dates': ['11/12/2015', '11/14/2015'],
+        'links': [5,6]}
     ];*/
+
     var j = jQuery.noConflict();
     j.ajax({
           method: "GET",
@@ -19,5 +20,12 @@ myapp.controller('historyCtrl', function($scope, $http) {
     .done(function(msg) {
         console.log(msg);
         $scope.data = msg;
+        for(var i = 0; i < $scope.data.length; i++) {
+            $scope.data[i].responses = [];
+            for (var j = 0; j < $scope.data[i]['dates'].length; j++) {
+              $scope.data[i].responses.push({'date': $scope.data[i]['dates'][j],
+                                             'link': $scope.data[i]['links'][j]});
+            }
+        }
     });
 });
