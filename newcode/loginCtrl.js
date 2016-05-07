c86014de9e358d1a8f229c596a17478577ab82eb
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2016-05-07 14:51:08 -0400

    login change

--- newcode/loginCtrl.js
+++ newcode/loginCtrl.js
@@ -27,26 +27,19 @@ angular.module('myapp').controller('loginCtrl', ['$scope', '$state', 'Authentica
 			    })
 			    .done(function(msg) {
 			        $scope.user = msg[0];
+			        console.log($scope.user);
 			        if ($scope.user.type == "client") {
 			        	$scope.user.type = 1;
+			        	$state.go('clientview');
 			        } else if ($scope.user.type == "lawyer") {
 			        	$scope.user.type = 2;
+			        	$state.go('clientlist');
 			        } else if ($scope.user.type == "admin") {
 			        	$scope.user.type = 8;
+			        	$state.go('admin');
 			        } else {
 			        	$scope.user.type = 4;
 			        }
-			        console.log($scope.user);
-			        if ($scope.user['type'] & 8) {
-						$state.go('admin');
-					} else if ($scope.user['viewee'].length > 0) {
-						$state.go('clientlist');
-					} else if ($scope.user['type'] & 1) {
-						$state.go('clientview');
-					} else {
-						console.log('error');
-					}
-					console.log($state);
 			    });
 		    } else {
 		    	$scope.reserror = response;
